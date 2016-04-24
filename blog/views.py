987170fdf7acdadfe66dc2adc0b6c2495b9e2926
derelektrischemoench <baederchris@gmail.com>
2016-04-24 12:25:22 +0200

    ...

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ from .forms import CommentForm
 
 
 def post_list(request):
-    ##ab hier additions 20160421
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)  # 3 posts per page
     page = request.GET.get('page')
@@ -29,14 +28,13 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
-                             #so far this keeps throwing errors... we need to fix it...or does it??
                              publish__year=year,
                              publish__month=month,
                              publish__day=day
                              )
 
     #include the comments from the model we created earlier
-    #list of active comments:
+    #queryset for filtering active comments:
     comments = post.comments.filter(active=True)
 
     if request.method == 'POST':
@@ -44,13 +42,12 @@ def post_detail(request, year, month, day, post):
         ##request into the form
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
+            new_comment = comment_form.save(commit=False)##create instance but don't save it yet
+            new_comment.post = post ##this links the form instance to the comment
+            new_comment.save()##save the whole thing to the DB
         ##if the request is empty, aka an empty GET request, Django automatically supplies an empty form
     else:
         comment_form = CommentForm()
-
 
     return render(request,
                   'blog/static/post/detail.html',
