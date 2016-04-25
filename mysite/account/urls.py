423420cb4a2e85fec569c5a516906aa4f43c929a
christian <christian.frank@schnapptack.de>
2016-04-25 17:42:05 +0200

    aksgdakjshgjdhsfadflga

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^logout-then-login/$',
         'django.contrib.auth.views.logout_then_login',
         name='logout_then_login'),
+    url(r'^$',views.dashboard, name='dashboard'),
 ]
