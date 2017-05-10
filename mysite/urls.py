562f6eee752c6f5c62aff631f6fe0d7f26486e84
Chris Bader <chris.bader@schnapptack.de>
2017-05-10 14:10:52 +0200

    fixed include error in urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from home import views
 
 urlpatterns = [
-    url(r'^$', 'home.views.index'),
+    url(r'^$', views.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',
                            namespace='blog',
